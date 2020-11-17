6401a5b69b2d508fdbd1ad7bb2aacb4e1ca4536b
bidisha12 <bidishapaul237@gmail.com>
2020-11-17 16:08:35 +0530

    new
    Signed-off-by: bidisha12 <bidishapaul237@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
@@ -9,9 +9,78 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    setBallPosition({
+      left: "0px",
+      top: "0px",
+    })
+  };
+  const checkRender= ()=> {setRenderBall(true)};
+  const renderChoice = () => {
+    if(renderBall===true)
+    {
+      return <div className="ball" style={ballPosition}></div>
+    }
+    else{
+      return <button className="start" onClick={checkRender}>Start</button>
+    }
+  };
 
+  const moveBall= (evt)=>{
+    if(renderBall===true)
+    {
+      if(evt.key=== "ArrowUp")
+      {
+        y-=5;
+        setY(y);
+        setBallPosition({
+          top: y + "px",
+          left: x + "px"
+        });
+      }
+      else if(evt.key=== "ArrowDown")
+      {
+        y+=5;
+        setY(y);
+        setBallPosition({
+          ...ballPosition,
+          top: y + "px",
+          left: x + "px"
+        });   
+      }
+
+      else if(evt.key=== "ArrowLeft")
+      {
+        x-=5;
+        setX(x);
+        setBallPosition({
+          ...ballPosition,
+          top: y + "px",
+          left: x + "px"
+        });   
+      }
+      else if(evt.key=== "ArrowRight")
+      {
+        x+=5;
+        setX(x);
+        setBallPosition({
+          ...ballPosition,
+          top: y + "px",
+          left: x + "px"
+        });   
+      }
+    }
+  }
+  useEffect(()=>{
+    window.addEventListener('keydown',moveBall);
+    return ()=>
+    {
+      window.addEventListener('keydown',moveBall);
+    };
+  });
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
